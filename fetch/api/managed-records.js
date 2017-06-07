c2f9655b0ce2b02cf4bb113109a1a5f8bba9daac
dominic <dominic.tracey@gmail.com>
2017-06-06 23:33:29 -0400

    middleware and fetch solutions

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -5,6 +5,74 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
+function retrieve(req) {
+  // transform the pagination params
+  const params = parseParams(req)
 
+  // base uri
+  var uri = URI(path).search(params)
+
+  // sorry, I couldn't get URI.js to not urlencode the colors
+  const colors = req && 'colors' in req ? req.colors : null
+  var colorParams = ''
+  if (colors) {
+    colorParams = colors.map(c => `&color[]=${c}`).join('')
+  }
+
+  return new Promise((resolve, reject) => {
+    var output = {}
+    const page = req && req.page ? req.page : 1
+    fetch(uri+colorParams)
+      .then(response => {
+        // get the response body as JSON
+        return response.json()
+      })
+      .then(json => {
+          // transform from the old json to new object
+          output.previousPage = getPreviousPage(page)
+          output.nextPage = getNextPage(page, json.length == 0)
+          output.ids = getIds(json)
+          output.open =  getOpen(json)
+          output.closedPrimaryCount = getClosedPrimaryCount(json)
+          resolve(output)
+      })
+      .catch(err => {
+        console.log('something bad happened: ' + err)
+        resolve(err)
+      })
+   })
+}
+
+function parseParams(req) {
+  var obj = { limit: 10 }
+  obj.offset = req && req.page ? (req.page-1)*10 : 0
+  return obj
+}
+
+function getIds(json) {
+  return json.slice(0,10).map(obj => obj.id)
+}
+
+function getOpen(json) {
+  return json.filter(obj => obj.disposition == 'open')
+              .map(obj => {
+                obj.isPrimary = ['red','yellow','blue'].indexOf(obj.color) >= 0
+                return obj
+              })
+}
+
+function getPreviousPage(page=1) {
+  return page > 1 ? page - 1 : null
+}
+
+function getNextPage(page=1, empty) {
+  return page < 50 && !empty ? page + 1 : null
+}
+
+function getClosedPrimaryCount(json) {
+  return json.reduce((acc,o) => {
+    return o.disposition == 'closed' && ['red','yellow','blue'].indexOf(o.color) >= 0 ? acc + 1 : acc
+  },0)
+}
 
 export default retrieve;
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httptest"
+	"sort"
 	"strconv"
 )
 
@@ -13,9 +17,54 @@ const (
 	colonspace = ": "
 )
 
+// ChecksumMiddleware inserts SHA-1 checksums and passes on to next handler
 func ChecksumMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// your code goes here ...
+		// create a fake writer so we can look ahead at the base headers
+		fakeW := httptest.NewRecorder()
+		h.ServeHTTP(fakeW, r)
+
+		can := fmt.Sprintf("%d", fakeW.Code)
+		can += crlf
+
+		// create an array of the header names for sorting
+		var keys []string
+		for k := range fakeW.Header() {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+
+		// add the sorted headers
+		headers := ""
+		i := 0
+		for _, key := range keys {
+			can += key + ": " + fakeW.Header().Get(key) + crlf
+			headers += key
+			// no trailing semicolon
+			if i < len(keys)-1 {
+				headers += ";"
+				i++
+			}
+		}
+
+		can += "X-Checksum-Headers: " + headers + crlf + crlf
+
+		body := fakeW.Body.String()
+		fmt.Println("BODY: " + body)
+		can += body
+
+		// now we can calculate the hash
+		hasher := sha1.New()
+		hasher.Write([]byte(can))
+		src := hasher.Sum(nil)
+		dst := make([]byte, hex.EncodedLen(len(src)))
+		hex.Encode(dst, src)
+
+		// insert checksum headers in the real response
+		w.Header().Set("X-Checksum-Headers", headers)
+		w.Header().Set("X-Checksum", string(dst))
+
+		h.ServeHTTP(w, r)
 	})
 }
 
